552b8eddb82e21108a9766f7f51379a19cc8230c
Amy Shelton <ashelton@protonmail.com>
2024-09-24 10:26:54 -0400

    more work on site

---
+++ js/custom-carousel.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const moveNext = (carouselElement, scrollPosition) => {
+        const cardWidth = carouselElement.querySelector(".carousel-item").offsetWidth;
+        const carouselWidth = carouselElement.querySelector(".carousel-inner").scrollWidth;
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            carouselElement.querySelector(".carousel-inner")
+                .scroll({
+                    left: scrollPosition,
+                    behavior: "smooth",
+                });
+        }
+        return scrollPosition;
+    };
+    const movePrev = (carouselElement, scrollPosition) => {
+        const cardWidth = carouselElement.querySelector(".carousel-item").offsetWidth;
+        const carouselWidth = carouselElement.querySelector(".carousel-inner").scrollWidth;
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            carouselElement.querySelector(".carousel-inner")
+                .scroll({
+                    left: scrollPosition,
+                    behavior: "smooth",
+                });
+        }
+        return scrollPosition;
+    };
+    const activateCarousel = (carouselElement) => {
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            let scrollPosition = 0;
+
+            const nextButton = carouselElement.querySelector(".carousel-control-next");
+            nextButton.addEventListener("click", () => {
+                scrollPosition = moveNext(carouselElement, scrollPosition)
+            });
+            nextButton.addEventListener("touchstart", () => {
+                scrollPosition = moveNext(carouselElement, scrollPosition);
+            })
+
+            const prevButton = carouselElement.querySelector(".carousel-control-prev");
+            prevButton.addEventListener("click", () => {
+                scrollPosition = movePrev(carouselElement, scrollPosition)
+            });
+            prevButton.addEventListener("touchstart", () => {
+                scrollPosition = movePrev(carouselElement, scrollPosition)
+            });
+        } else {
+            carouselElement.classList.add("slide");
+        }
+    };
+
+    var carousels = document.querySelectorAll(".carousel");
+    carousels.forEach(activateCarousel);
+});
